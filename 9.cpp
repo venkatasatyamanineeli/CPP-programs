52fe16108e0b67af2528a5a5bdd2cec06d6fcd2e
venkata satyamani Neeli <venkatasatyamanineeli@gmail.com>
2026-08-13 11:19:21 +0530

    Add files via upload

---
+++ 9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+
+    sum = a + b;
+
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
